8f52c4bc8e5ca5eb931737d8d7183531debfcc2e
Coranna Howard <me@komiga.com>
2015-03-23 00:31:27 -0400

    igen: use custom visitor instead of cindex.Cursor.get_children().¹
    1: cindex.Cursor.get_children() returns an iterator instead of the
    children list.

--- src/igen.py
+++ src/igen.py
@@ -7,10 +7,30 @@ global opts
 global G_children_cache
 G_children_cache = {}
 
+def visit(cursor, visitor, children):
+	cindex.conf.lib.clang_visitChildren(
+		cursor, cindex.callbacks['cursor_visit'](visitor), children
+	)
+
+# Unfortunately, cindex.Cursor.get_children() gives an iterator instead of
+# the list.
+def Cursor_get_children(cursor):
+	def visitor(child, parent, children):
+		assert child != cindex.conf.lib.clang_getNullCursor()
+		child._tu = cursor._tu
+		children.append(child)
+		return 1 # continue
+
+	children = []
+	visit(cursor, visitor, children)
+	return children
+
 def get_children(cursor):
 	children = G_children_cache.get(cursor.hash, None)
+	# print("hit: %s / %s %d %s" % (children and "yes" or "no ", cursor.kind, cursor.hash, cursor.spelling))
 	if not children:
-		children = G_children_cache[cursor.hash] = cursor.get_children()
+		children = Cursor_get_children(cursor)
+		G_children_cache[cursor.hash] = children
 	return children
 
 def get_annotations(cursor):
