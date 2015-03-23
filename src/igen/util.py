534a337c54d94cd02120af578bb6cc35fe6eff11
Coranna Howard <me@komiga.com>
2015-03-23 04:11:45 -0400

    igen/util: added until param to fully_qualified_name_parts().

--- src/igen/util.py
+++ src/igen/util.py
@@ -37,17 +37,17 @@ def get_annotations(cursor):
 		if c.kind == CursorKind.ANNOTATE_ATTR
 	]
 
-def fully_qualified_name_parts(cursor):
+def fully_qualified_name_parts(cursor, until = None):
 	parts = [cursor.spelling]
 	p = cursor
 	while True:
 		p = p.semantic_parent
 		# print("%s: %s" % (p.kind, p.spelling))
 		# TODO: Other valid parts?
-		if not p or p.kind != CursorKind.NAMESPACE:
+		if not p or p.kind != CursorKind.NAMESPACE or (until and p.spelling == until):
 			break
 		parts.append(p.spelling)
 	return parts[::-1]
 
-def fully_qualified_name(cursor, parts = None):
-	return "::".join(parts or fully_qualified_name_parts(cursor))
+def fully_qualified_name(cursor, until = None, parts = None):
+	return "::".join(parts or fully_qualified_name_parts(cursor, until))
